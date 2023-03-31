ef0ca890f97b90e91a6c2660a93eaf4d265f83e7
Joeun Park <park49j@mtholyoke.edu>
2023-03-30 20:50:13 -0400

    designed skeleton

--- react-chrome-app/src/App.js
+++ react-chrome-app/src/App.js
@@ -1,6 +1,17 @@
 // function App() {
+//   const appStyles = {
+//     position: "fixed",
+//     top: "10px",
+//     right: "10px",
+//     zIndex: 1000,
+//     backgroundColor: "white",
+//     padding: "10px",
+//     boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+//     borderRadius: "4px",
+//   };
+
 //   return (
-//     <div>
+//     <div style={appStyles}>
 //       <h1>React Chrome Extension</h1>
 //     </div>
 //   );
@@ -8,21 +19,58 @@
 
 // export default App;
 
+import React, { useState } from 'react';
+import './App.css';
+
 function App() {
-  const appStyles = {
-    position: "fixed",
-    top: "10px",
-    right: "10px",
-    zIndex: 1000,
-    backgroundColor: "white",
-    padding: "10px",
-    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-    borderRadius: "4px",
+  const [selectedTones, setSelectedTones] = useState([]);
+  const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState('');
+
+  const tones = ['Neutral', 'Formal', 'Smart', 'Concise', 'Professional', 'Firm'];
+
+  const handleToneClick = (tone) => {
+    if (selectedTones.includes(tone)) {
+      setSelectedTones(selectedTones.filter((t) => t !== tone));
+    } else {
+      setSelectedTones([...selectedTones, tone]);
+    }
+  };
+
+  const handleGenerateClick = async () => {
+    // Send API request using the prompt text input data
+    // For demonstration purposes, I'll just use a setTimeout to simulate an API call.
+    setTimeout(() => {
+      setResponse(`Generated response for prompt: "${prompt}" with tones: ${selectedTones.join(', ')}`);
+    }, 1000);
   };
 
   return (
-    <div style={appStyles}>
+    <div className="app-modal">
       <h1>React Chrome Extension</h1>
+      <input
+        type="text"
+        placeholder="Enter your prompt"
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <div>
+        {tones.map((tone) => (
+          <button
+            key={tone}
+            className={`app-button${selectedTones.includes(tone) ? ' selected' : ''}`}
+            onClick={() => handleToneClick(tone)}
+          >
+            {tone}
+          </button>
+        ))}
+      </div>
+      <button className="app-button" onClick={handleGenerateClick}>
+        Generate
+      </button>
+      {response && (
+        <textarea className="textarea" value={response} readOnly />
+      )}
     </div>
   );
 }
--- react-chrome-app/webpack.config.js
+++ react-chrome-app/webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: {
