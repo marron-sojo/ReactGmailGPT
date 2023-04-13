4d1f3942b86c6df7a70183de801247823bbde215
Joeun Park <park49j@mtholyoke.edu>
2023-04-13 19:11:14 -0400

    bug fix: Ensure that only one button is visible in each separate window

--- react-chrome-app/src/index.js
+++ react-chrome-app/src/index.js
@@ -2,31 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// create a new MutationObserver object and pass it a callback function
 const observer = new MutationObserver(function (mutationsList, observer) {
-  // iterate through each mutation in the mutations list
   for (const mutation of mutationsList) {
-    // check if a new element has been added to the body
     if (
       mutation.type === "childList" &&
       mutation.addedNodes.length > 0 &&
       mutation.target === document.body
     ) {
-      // do something with the new element, such as log a message to the console
-      //   console.log('A new element was added to the body:', mutation.addedNodes[0]);
-
       const addedNode = mutation.addedNodes[0];
       const newWindow = addedNode.closest('div[aria-label="New Message"]');
-      console.log(newWindow);
+
       if (newWindow) {
-        const sendButton = document.querySelector('div[data-tooltip*="Send"]');
+        const sendButton = newWindow.querySelector('div[data-tooltip*="Send"]');
         // Create a new icon element
         const icon = document.createElement("div");
-        icon.innerHTML = "🦧";
+        icon.innerHTML = "🏆";
         icon.title = "Click here to launch your email!";
+        icon.className = "icon-btn"; // Add a class name to the icon element
 
-        // Add the icon to the parent element of the Send button
-        // sendButton.parentNode.insertBefore(icon, sendButton);
         if (
           sendButton &&
           sendButton.parentNode &&
@@ -43,37 +36,22 @@ const observer = new MutationObserver(function (mutationsList, observer) {
   }
 });
 
-
 let isModalShowing = false;
 
 document.addEventListener('click', function(e) {
-    // if '🚀' is clicked, <App/> component appears
-    if (e.target.innerHTML === "🦧" && !isModalShowing) {
-      // Create a new div element and give it an id
-      const appRoot = document.createElement("div");
-      appRoot.id = "app-root";
-      document.body.appendChild(appRoot);
-  
-      const root = ReactDOM.createRoot(appRoot);
-      root.render(
-        <React.StrictMode>
-          {/* <App /> */}
-          <App onClose={() => document.getElementById("app-root").remove()} />
-        </React.StrictMode>
-      );
-      isModalShowing = true;
-    }
+  if (e.target.innerHTML === "🏆" && !isModalShowing) {
+    const appRoot = document.createElement("div");
+    appRoot.id = "app-root";
+    document.body.appendChild(appRoot);
 
-
+    const root = ReactDOM.createRoot(appRoot);
+    root.render(
+      <React.StrictMode>
+        <App onClose={() => document.getElementById("app-root").remove()} />
+      </React.StrictMode>
+    );
+    isModalShowing = true;
+  }
 });
 
-
-// call the observer's observe method to start watching for changes to the DOM
 observer.observe(document.body, { childList: true });
-
-// const root = ReactDOM.createRoot(rootElement);
-// root.render(
-//   <React.StrictMode>
-//     {/* <App /> */}
-//   </React.StrictMode>
-// );
